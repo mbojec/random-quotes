b0b14fe91e278c071eee0527ca126536587dd86e
Marek Bojec <marek.bojec@gmail.com>
2019-08-05 17:19:01 +0200

    Creating and styling footer section.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import GithubLogo from "./resources/svg/githubLogo";
+import LinkedinLogo from "./resources/svg/linkedinLogo";
+import GmailLogo from "./resources/svg/gmailLogo";
 
 function Footer() {
-  return(<div className={'footer'}></div>)
+  return(
+    <div className={'footer'}>
+      <div className={'row'}>
+        <div className={'col-xs-12'}><h2>Contact</h2></div>
+        <div className={'col-xs-12 footer__icons-container'}>
+          <a title={'mbojec'} href={'https://github.com/mbojec'}><GithubLogo/></a>
+          <a title={'Marek Bojęć'} href={'https://www.linkedin.com/in/marek-boj%C4%99%C4%87-425b68117/'}><LinkedinLogo/></a>
+          <a title={'mbojec.dev@gmail.com'} href={'mailto:mbojec.dev@gmail.com'}><GmailLogo/></a>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
