8e4347df0a319207d2dbcc7a04c3ed8277b36b7a
Marek Bojec <marek.bojec@gmail.com>
2019-08-05 17:25:08 +0200

    Creating and styling basic app bar

--- src/AppBar.jsx
+++ src/AppBar.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 function AppBar() {
   return (
-    <div className={'app-bar'}></div>
+    <div className={'app-bar'}>
+      <div className={'row'}>
+        <div className={'col-xs-12'}>
+          <h2>Random Quotes</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
