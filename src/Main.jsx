99edc99d70b239daed2e4cb8cd9e54bca03ad5f4
Marek Bojec <marek.bojec@gmail.com>
2019-08-06 08:31:55 +0200

    Styling Main layout and adding redo icon

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import {faRedo} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Main() {
   return(
     <div className={'main'}>
-      <div className={'row'}>
-        <div className={'col-xs-12 main__blockquote'}>
+        <div className={'main__blockquote col-xs-12'}>
           <blockquote cite={'https://theysaidso.com/quote/sally-blount-if-we-ever-stop-thinking-about-the-person-we-want-to-become-we-stop'}>
             <p>"If we ever stop thinking about the person we want to become, we stop learning and taking risks."</p>
             <span>Sally Blount</span>
           </blockquote>
         </div>
-        <div className={'col-xs-12 main__ui-panel'}>
-
+        <div className={'main__ui-panel col-xs-12'}>
+          <FontAwesomeIcon icon={faRedo}/>
         </div>
-      </div>
-    </div>)
+    </div>
+  )
 }
 
 export default Main
