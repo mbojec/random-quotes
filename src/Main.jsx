ba7e28cb060cf67aee92696d88ce5b2aacbd19f0
Marek Bojec <marek.bojec@gmail.com>
2019-08-06 06:37:28 +0200

    Adding sample blockquote with styling

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 function Main() {
   return(
     <div className={'main'}>
+      <div className={'row'}>
+        <div className={'col-xs-12 main__blockquote'}>
+          <blockquote cite={'https://theysaidso.com/quote/sally-blount-if-we-ever-stop-thinking-about-the-person-we-want-to-become-we-stop'}>
+            <p>"If we ever stop thinking about the person we want to become, we stop learning and taking risks."</p>
+            <span>Sally Blount</span>
+          </blockquote>
+        </div>
+        <div className={'col-xs-12 main__ui-panel'}>
 
+        </div>
+      </div>
     </div>)
 }
 
